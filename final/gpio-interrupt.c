b768e13ad2c57041baf28a675091962566095ce8
Tom Xue <tomxue0126@gmail.com>
2014-06-04 11:52:02 +0800

    formatted

--- final/gpio-interrupt.c
+++ final/gpio-interrupt.c
@@ -15,57 +15,57 @@ static volatile int state;
 struct timeval last_change;
 
 // Handler for interrupt
-void handle(void) {
-	struct timeval now;
-	unsigned long diff;
+void handle(void)
+{
+    struct timeval now;
+    unsigned long diff;
 
-	gettimeofday(&now, NULL);
+    gettimeofday(&now, NULL);
 
-	// Time difference in usec
-	diff = (now.tv_sec * 1000000 + now.tv_usec) - (last_change.tv_sec * 1000000 + last_change.tv_usec);
+    // Time difference in usec
+    diff = (now.tv_sec * 1000000 + now.tv_usec) - (last_change.tv_sec * 1000000 + last_change.tv_usec);
 
-	// Filter jitter
-	if (diff > IGNORE_CHANGE_BELOW_USEC) {
-		if (state) {
-			printf("Falling\n");
+    // Filter jitter
+    if (diff > IGNORE_CHANGE_BELOW_USEC) {
+        if (state) {
+            printf("Falling\n");
             system("./leds-red.sh");
-		}
-		else {
-			printf("Rising\n");
+        } else {
+            printf("Rising\n");
             system("./leds-blue.sh");
-		}
+        }
 
-		state = !state;
-	}
+        state = !state;
+    }
 
-	last_change = now;
+    last_change = now;
 }
 
-int main(void) {
-	// Init
-	wiringPiSetup();
+int main(void)
+{
+    // Init
+    wiringPiSetup();
 
-	// Set pin to output in case it's not
-	pinMode(PIN, OUTPUT);
+    // Set pin to output in case it's not
+    pinMode(PIN, OUTPUT);
 
-	// Time now
-	gettimeofday(&last_change, NULL);
+    // Time now
+    gettimeofday(&last_change, NULL);
 
-	// Bind to interrupt
-	wiringPiISR(PIN, INT_EDGE_BOTH, &handle);
+    // Bind to interrupt
+    wiringPiISR(PIN, INT_EDGE_BOTH, &handle);
 
-	// Get initial state of pin
-	state = digitalRead(PIN);
+    // Get initial state of pin
+    state = digitalRead(PIN);
 
-	if (state) {
-		printf("Started! Initial state is on\n");
-	}
-	else {
-		printf("Started! Initial state is off\n");
-	}
+    if (state) {
+        printf("Started! Initial state is on\n");
+    } else {
+        printf("Started! Initial state is off\n");
+    }
 
-	// Waste time but not CPU
-	for (;;) {
-		sleep(1);
-	}
+    // Waste time but not CPU
+    for (;;) {
+        sleep(1);
+    }
 }
